8d9b8801b44ccb3aa41d852d92f4b5cea08d6995
Bill Mårtensson <bill@magictechnology.se>
2020-11-26 12:50:41 +0100

    Initial Commit

---
+++ Komplettappen/KomplettappenApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomplettappenApp.swift
+//  Komplettappen
+//
+//  Created by Bill Martensson on 2020-11-26.
+//
+
+import SwiftUI
+
+@main
+struct KomplettappenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
